722d07247bca3af735c823a29657efbdeee98dac
ZIHCO <chizobajames21@gmail.com>
2023-12-16 04:22:42 +0100

    fixing error in syntax

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- SQL script that creates a table users
 
 CREATE TABLE IF NOT EXISTS users (
-		    id INT UNIQUE AUTO_INCREMENT PRIMARY KEY,
-		    email VARCHAR(255) UNIQUE,
+		    id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+		    email VARCHAR(255) UNIQUE NOT NULL,
 		    name VARCHAR(255));
