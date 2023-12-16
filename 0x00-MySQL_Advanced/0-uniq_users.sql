0986b76942cb17f0224eed0a9dc8d2aa28d9247f
ZIHCO <chizobajames21@gmail.com>
2023-12-16 03:55:04 +0100

    add readme

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- SQL script that creates a table users
 
 CREATE TABLE IF NOT EXISTS users (
-		    id INT AUTO_INCREMENT UNIQUE PRIMARY_KEY,
+		    id INT UNIQUE PRIMARY_KEY,
 		    email VARCHAR(255) UNIQUE,
 		    name VARCHAR(255));
