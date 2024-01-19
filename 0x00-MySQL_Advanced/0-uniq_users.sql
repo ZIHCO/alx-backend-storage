3cb5c931acd308b3260aea9067bcd15bafc205e2
ZIHCO <chizobajames21@gmail.com>
2024-01-19 03:12:36 +0100

    old repo deleted

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++
@@ -1,6 +0,0 @@
--- SQL script that creates a table users
-
-CREATE TABLE IF NOT EXISTS users (
-		    id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
-		    email VARCHAR(255) UNIQUE NOT NULL,
-		    name VARCHAR(255));
