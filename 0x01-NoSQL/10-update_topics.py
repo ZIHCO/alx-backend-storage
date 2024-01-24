53fa4a967dffc27824f8c9eadc1361ac50c08856
zihco <chizobajames21@gmail.com>
2024-01-24 15:12:00 +0100

    update many

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,8 +6,8 @@ def update_topics(mongo_collection, name, topics):
     """changes a doc attr"""
     if mongo_collection is not None:
         filter_criteria = {'name': name}
-        unset_old_attri = {$unset: 'topics'}
-        set_attri = {$set: {"topics": topics}}
+        unset_old_attri = {"$unset": 'topics'}
+        set_attri = {"$set": {"topics": topics}}
         list_named_topic = mongo_collection.update_many(
                                                         filter_criteria,
                                                         unset_old_attri,
