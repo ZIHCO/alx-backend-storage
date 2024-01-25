9dc7542f5c6f9391b2b9b80ff270bdaa6f979632
zihco <chizobajames21@gmail.com>
2024-01-25 05:03:48 +0100

    writing string to redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""writing strings to Redis"""
+from redis import Redis
+from typing import Any
+import uuid
+
+
+class Cache:
+    """defines a redis object"""
+
+    def __init__(self):
+        """instantiate a Redis client object"""
+        self._redis = Redis()
+        self._redis.flushdb
+
+    def store(self, data: Any[str | bytes | float]) -> str:
+        """returns the key"""
+        key = uuid.uuid4()
+        self._redis.set({key: data})
+        return key
