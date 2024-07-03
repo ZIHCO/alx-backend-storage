ebfa042ad6ed2caa7cafd3fecd5f3f55fa9fd19e
zihco <chizobajames21@gmail.com>
2024-07-03 06:42:31 +0100

    done

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -65,6 +65,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """returns the key"""
         key = uuid.uuid4().hex
